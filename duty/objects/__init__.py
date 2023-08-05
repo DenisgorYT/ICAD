65651ed822dd3cee390fc4655fc2f10f014c991a
Denis Gorbatsov <97974572+DenisgorYT@users.noreply.github.com>
2023-08-05 17:06:58 +0300

    Update __init__.py

--- duty/objects/__init__.py
+++ duty/objects/__init__.py
@@ -1,7 +1,7 @@
 try:
     from ._version import __version__
 except ImportError:
-    __version__ = __А те зачем?__'
+    __version__ = __Хз__'
 
 from .events import *
 from . import dispatcher as dp
